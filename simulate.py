11a61eccb613e56731bfeb5d735a2aa9ea71e089
Ollie <oli@flowmo.co>
2022-03-29 16:26:03 +0100

    changed sim attributes

--- simulate.py
+++ simulate.py
@@ -1,5 +1,15 @@
 # Template
 
 class Simulation:
+
     def __init__(duration):
+        
         self.duration = duration
+        self.day = 0
+
+        # just template probabilites, will change
+        self.recovery_probability = 0.7
+        self.infection_probability = 0.1
+        self.death_probability = 0.05
+
+    

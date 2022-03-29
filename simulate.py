f519eadf22f732768c236ff372781fae6fe8a271
Derek Sum <ab20466@bristol.ac.uk>
2022-03-29 16:26:17 +0100

    Added world class

--- simulate.py
+++ simulate.py
@@ -1,8 +1,14 @@
 # Template
 
+import numpy as np
+from matplotlib import pyplot as plt
+'''
 class Simulation:
     def __init__(time, size):
-        
+  '''      
 
+fig = plt.figure(figsize=(10,10))
+plt.xlim(0,100)
+plt.ylim(0,100)
 
     

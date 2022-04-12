a4e221acf7dd2cf7fb2a468114d74d461fd556d2
Derek Sum <ab20466@bristol.ac.uk>
2022-04-12 21:00:45 +0100

    the original world doesnt world, i therefore changed it to a new version which consist of two class( Animation and world)

--- simulate.py
+++ simulate.py
@@ -14,19 +14,30 @@ class Simulation:
         self.recovery_probability = 0.7
         self.infection_probability = 0.1
         self.death_probability = 0.05
+        
+class Animation:
+    #class for animation
+    def __init__(self):
+        #set the size of the figure
+        self.fig=plt.figure(figsize=(8,8))
+        #this is a sub plot allowing two figure in one run, can be changed to add another plot
+        self.axes_world = self.fig.add_subplot(1, 1, 1)
+        self.world = world(self.axes_world)
+       
+    
 class world:
-    def __init__(self,width,height,xlim,ylim):
-        self.width=width
-        self.height=height
-        self.xlim=xlim
-        self.ylim=ylim
-    def model(self):
-        self.fig = plt.figure(figsize=(self.width,self.height))
-        self.plt.xlim(0,self.xlim)
-        self.plt.ylim(0,self.ylim)
+    def __init__(self,axes):
+        
+        #hides the x, y axis of animation      
+        self.axes=axes
+        self.axes.set_xticks([])
+        self.axes.set_yticks([])
+'''
+test to see whether the world works
+x=Animation()
+print(x)
 
-
-
-
+'''
 
     
+

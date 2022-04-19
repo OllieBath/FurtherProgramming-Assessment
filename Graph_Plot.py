7c155c61705071fa3eadde1e871a5964d644ab84
KareemElsabbagh <rg21394@bristol.ac.uk>
2022-04-19 16:33:40 +0200

    cleaning up unnecessary lines

--- Graph_Plot.py
+++ Graph_Plot.py
@@ -33,27 +33,24 @@ vaccinated=slicef["vaccinated"]
 
 # print(vaccinated)
 def dplot(x,y1,y2,x_label,y1_label,y2_label,title):
-    fig1,new=plt.subplots(figsize=(10,12))
-    new.plot(x,y1,'r-')
-    new.set_xlabel(x_label)
+    fig1,first=plt.subplots(figsize=(10,12))
+    first.plot(x,y1,'r-')
+    first.set_xlabel(x_label)
     # ax1.xticks(np.arange(10,800),50)
-    new.set_ylabel(y1_label,fontsize=12)
-    new.set_xlim(0,800)
-    # new.set_ylim(0,300000)
-    cum=new.twinx()
-    cum.plot(x,y2,'-b',)
-    # cum.set_ylim(0,22000000)
-    cum.set_yscale("linear")
-    cum.set_ylabel(y2_label,fontsize=12)
+    first.set_ylabel(y1_label,fontsize=12)
+    first.set_xlim(0,800)
+    sec=first.twinx()
+    sec.plot(x,y2,'-b',)
+    sec.set_yscale("linear")
+    sec.set_ylabel(y2_label,fontsize=12)
     
-    cum.set_title(title)
+    sec.set_title(title)
     red_patch = mpatches.Patch(color='red', label=y1_label)
     blue_patch = mpatches.Patch(color='blue', label=y2_label)
     plt.legend(handles=[red_patch]+[blue_patch],loc=("upper left"))
     plt.savefig(title)
 
-dplot(days,newcases,newdeaths,"Dayssssss","New casess","New Deaths","title")
-dplot(days,vaccinated,newdeaths,"Dayssssss","vaccinated","Newdeaths","title2")
+
 
 
 

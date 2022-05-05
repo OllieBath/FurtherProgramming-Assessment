c05728fb50da1d048ce2271abd112a36348573c4
Kareem Elsabbagh <98831378+KareemElsabbagh@users.noreply.github.com>
2022-05-05 20:54:44 +0100

    Update realcovidplot.py

--- realcovidplot.py
+++ realcovidplot.py
@@ -59,6 +59,6 @@ def animate(i):
     sec.plot(x,y2, scaley=True, scalex=True, color="red")
     fst.plot(x,y3, scaley=True, scalex=True, color="green") 
 
-ani = FuncAnimation(fig=fig, func=animate,frames=800, interval=0.0001)
+ani = FuncAnimation(fig=fig, func=animate,frames=800, interval=0.0001, repeat=False)
 ani.save("vid.mp4")    
 
